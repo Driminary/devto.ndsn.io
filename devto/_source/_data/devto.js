22cca1b18ac2b0bed58080b8e816ccb7430eb1c4
Thomas Anderson <me@thomas-anderson.net>
2020-07-27 20:34:04 +0100

    Update devto.js

--- devto/_source/_data/devto.js
+++ devto/_source/_data/devto.js
@@ -5,17 +5,16 @@ var feed = process.env.DEVTO_FEED || "https://dev.to/feed/ndsn";
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    let response = axios.get(feed)
-    .then((response) => {
-    toJSON(response.data, { explicitArray: false, trim: true }, function (
-      err,
-      result
-    ) {
-      result.rss.channel.item.forEach((element) => {
-        element.path = element.link.split("/").slice(-1)[0];
+    axios.get(feed).then((response) => {
+      toJSON(response.data, { explicitArray: false, trim: true }, function (
+        err,
+        result
+      ) {
+        result.rss.channel.item.forEach((element) => {
+          element.path = element.link.split("/").slice(-1)[0];
+        });
+        resolve({ feed: feed, posts: result.rss.channel.item });
       });
-      resolve({ feed: feed, posts: result.rss.channel.item });
     });
   });
-});
 };

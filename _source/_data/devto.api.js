b46a59f1ecfb78881f66d0a46361586495ab6525
Tom Anderson <me@thomas-anderson.net>
2020-07-28 09:40:50 +0100

    API testing

---
+++ _source/_data/devto.api.js
@@ -0,0 +1,20 @@
+var axios = require("axios");
+
+var api_key = process.env.DEVTO_APIKEY || (() => { throw("No API Key Set!")})();
+var api_url = process.env.DEVTO_APIURL || "https://dev.to/api/articles/me";
+
+module.exports = (async () => {
+    try {
+    let response = await axios
+      .get(api_url, {
+        headers: {
+          'api-key': api_key, // Send the API Key in the header
+        },
+      })
+
+    return response.data
+
+    } catch(e) {
+        console.error(e)
+    }
+  })();

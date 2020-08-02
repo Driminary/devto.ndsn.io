1fad670ded76c146480b08e0a53aafbbc66f98ce
Thomas Anderson <me@thomas-anderson.net>
2020-08-02 18:24:37 +0100

    Update devtoapi.js

--- _source/_data/devtoapi.js
+++ _source/_data/devtoapi.js
@@ -1,11 +1,11 @@
 var axios = require("axios");
 var api_url = process.env.DEVTO_APIURL || "https://dev.to/api/articles?username=ndsn";
 
-module.exports = (async () => {
+module.exports = async () => {
   try {
     let response = await axios.get(api_url);
     return { feed: api_url, posts: response.data };
   } catch (e) {
-    console.error(e);
+    console.error(e.message);
   }
-})();
+});
